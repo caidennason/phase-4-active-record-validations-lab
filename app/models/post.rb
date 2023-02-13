a339a15712d952c076f93846e721a9029c81fdcc
caiden nason <caidenbnason@gmail.com>
2023-02-13 13:20:22 -0500

    commit tree e2e8bbaa4663fea7900df96dc879e6c00a4dea02

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :clickbait_post
+
+    def clickbait_post
+        # byebug
+        if title == "" || title == nil
+            false
+        else 
+            unless title.include?("Won't Believe") || title.include?("Secret")
+                errors.add(:title, "Not Good Enough")
+            end
+        end
+    end
+
 end
